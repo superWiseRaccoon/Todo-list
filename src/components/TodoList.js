d8b850e23288550b5b36fcfb0d028ff8af0fa6e6
superWiseRaccoon <haseohyeon43@gmail.com>
2024-04-19 20:25:09 +0900

    수정기능 구현완료.css손보기

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -8,7 +8,9 @@ const TodoList = () => {
   const [toDo, setTodo] = useState({
     text: '',
     id: '',
-    checked: false
+    checked: false,
+    editOn: false,
+    editText: ''
   });
 
   const handleInput = (e) => {
@@ -32,21 +34,67 @@ const TodoList = () => {
     setTodo(newInput);
   }
 
-  const activeEnter = (e) => {
+  const update = (key) => {
+    const updateValue = toDoList.map((item) => {
+      if (key === item.id) {
+        return {
+          ...item,
+          text: toDo.editText,
+          editOn: false
+        }
+      } else {
+        return item
+      }
+    })
+    setToDoList(updateValue);
+  }
+
+  const activeEnter = (e, type, key) => {
     if (e.key === "Enter") {
-      submit();
+      switch (type) {
+        case 'submit':
+          submit();
+          break;
+        case 'update':
+          update(key);
+          break;
+        default:
+          break;
+      }
     }
   }
 
   const handleCheck = (key) => {
     const newList = toDoList.map((item) => {
-      if (item.id === key) {     
+      if (item.id === key) {
         return {
           ...item,
           checked: !item.checked
         }
       } else {
         return item;
+      }
+    });
+    setToDoList(newList);
+  }
+
+  const handleEditSwitch = (key) => {
+    const newList = toDoList.map((item) => {
+      if (item.id === key) {
+        const newTodo = {
+          ...toDo,
+          editText: item.text
+        }
+        setTodo(newTodo);
+        return {
+          ...item,
+          editOn: !item.editOn
+        }
+      } else {
+        return {
+          ...item,
+          editOn: false
+        };
       }
     });
     setToDoList(newList);
@@ -65,7 +113,7 @@ const TodoList = () => {
       <div id='header'>
         <div id='title' className='center'>Todo-list</div>
         <div id='submitBox'>
-          <input name='text' type='text' value={toDo.text} spellCheck='false' onChange={(e) => handleInput(e)} onKeyDown={(e) => activeEnter(e)} />
+          <input name='text' type='text' value={toDo.text} spellCheck='false' onChange={(e) => handleInput(e)} onKeyDown={(e) => activeEnter(e, 'submit')} />
           <button type='button' onClick={() => submit()}>+</button>
         </div>
       </div>
@@ -76,15 +124,26 @@ const TodoList = () => {
               <div className='item' key={item.id}>
                 <div className='textBox'>
                   <input type='checkbox' id={item.id} onChange={() => handleCheck(item.id)} />
-                  {/* <label htmlFor={item.id}
-                    style={{color: item.checked && 'rgb(21, 87, 255)',
-                  textDecoration: item.checked && 'line-through'}}
-                  >{item.text}</label> */}
-                  <input type='text' className='editBox' value={item.text}/>
+                  {
+                    item.editOn ?
+
+                      <input name='editText' type='text' className='editBox' value={toDo.editText} autoFocus spellCheck="false" onChange={(e) => handleInput(e)} onKeyDown={(e) => activeEnter(e, 'update', item.id)} /> :
+
+                      <label htmlFor={item.id}
+                        style={{
+                          color: item.checked && 'rgb(21, 87, 255)',
+                          textDecoration: item.checked && 'line-through'
+                        }}
+                      >{item.text}</label>
+                  }
                 </div>
                 <div className='btnBox'>
-                  <button type='button' onClick={() => handleDelete()}>수정</button>
-                  <button type='button' onClick={() => handleDelete(item.id)}>삭제</button>
+                  {
+                    item.editOn ?
+                      <><button type='button' onClick={() => update(item.id)}>완료</button><button type='button' onClick={() => handleEditSwitch(item.id)}>취소</button></>
+                      :
+                      <><button type='button' onClick={() => handleEditSwitch(item.id)}>수정</button><button type='button' onClick={() => handleDelete(item.id)}>삭제</button></>
+                  }
                 </div>
               </div>
             )
